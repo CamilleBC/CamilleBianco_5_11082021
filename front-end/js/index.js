db445503400fb9024f1113102551073f0b764c24
Camille Bianco <bianco.camille@live.fr>
2021-08-26 10:47:19 +0200

    Renvoi carte dans la page produit : Avec choix dynamique de la couleur et de la quantité

--- front-end/js/index.js
+++ front-end/js/index.js
@@ -8,12 +8,13 @@ fetch('http://localhost:3000/api/teddies')
     resJson.forEach(item => {
         const DIV  = document.getElementById('items') ;
         DIV.innerHTML += `
-        <div class="card col-6 border-3 bg-brown">
-          <h5 class="card-title text-center">${item.name}</h5>
+        <div class="card col-5 m-auto" style="width : 20rem">
+          <h5 class="card-title text-center bg-warning p-2 mt-1 text-white">${item.name}</h5>
           <img class="card-img-top w-75 h-50 mx-auto" src="${item.imageUrl}">
-          <div class="card-body">
+          <div class="card-body d-flex flex-column">
               <p class"card-text">${item.description}</p>
-              <p class"card-text">${item.price} €</p>
+              <div class"card-text">${item.price} €</div>
+              <a class"btn btn-primary" href="./pages/produit.html?_id=${item._id}">Voir plus</a>
           </div>
         </div>
         `;     
---
+++ front-end/js/item.js
@@ -0,0 +1,46 @@
+let id = window.location.search.substr(5);
+let url = 'http://localhost:3000/api/teddies/'+ id;
+
+
+fetch(url)
+    .then(function(res) {
+        if (res.ok) {
+          return res.json();
+        }
+      })
+      .then(function(resJson) {
+            console.log(resJson);
+            const DIV  = document.getElementById('produit') ;
+            DIV.innerHTML += `
+            <div class="card h-50 w-75 mx-auto">
+              <h5 class="card-title text-center bg-warning p-2 mt-1 text-white">${resJson.name}</h5>
+              <img class="card-img-top w-75 h-50 mx-auto" src="${resJson.imageUrl}">
+              <div class="card-body d-flex flex-column">
+                <p class"card-text">${resJson.description}</p>
+                <div class"card-text">${resJson.price} €</div>
+              <div class="card-text d-flex">
+                <p class="mt-3">Quantité</p>
+                <input type="number" name="quantité" value="1" class="tailleSaisie m-2">
+              </div>
+              <div class="card-text d-flex">
+                <p class="mt-3">Couleur</p>
+                <select id="color" class="tailleSelect m-2">
+                </select>
+              </div>
+              </div>
+              <a href="./panier.html">Ajouter au panier</a>
+            </div>
+            `;
+            const select = document.getElementById('color');
+            let tab = resJson.colors;
+            for(color of tab){
+               let option = document.createElement('option');
+               option.innerHTML = color;
+               select.appendChild(option)
+            }
+        
+      })
+      
+      .catch(function(err) {
+        // Une erreur est survenue
+      });
